b9eabce2eb2b0724ddbfd5340b5c727262277fd4
Deepesh Gotia <deepeshgotiya79@gmail.com>
2024-08-04 18:30:21 +0530

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :admins
+  devise_for :users
+ 
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
+end
